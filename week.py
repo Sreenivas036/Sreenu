6d955bdb8cb93753215a910bc74bd45b3de7ffcc
c Sreenivas <csreenivasuluseenu036@gmail.com>
2024-03-15 12:02:10 +0530

    test program on week

---
+++ week.py
@@ -0,0 +1,20 @@
+print("TO DISPLAY THE DAY IN WEEK")
+print("\n\n**************************************")
+num=int(input("enter a value:"))
+if(num==1):
+    print("\n sunday",num)
+elif(num==2):
+    print("\n monday",num)
+elif(num==3):
+    print("\n  tuesday",num)
+elif(num==4):
+    print("\n wednesday",num)
+elif(num==5):
+    print("\n thursday",num)
+elif(num==6):
+    print("\n  friday",num)
+elif(num==7):
+    print("\n  saturday",num)
+else:
+    print("day is not valid")
+
